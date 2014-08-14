c7d00c68b28c78bf19b3651dd3a355e944f0b0aa
Emily Kelton <emilyk000@gmail.com>
2014-08-14 11:26:31 -0700

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+
+
+// on page load
+// 	show instructions
+
+// on game start
+// 	fade instructions
+// 	show 1st question
+// 	show answer choices
+// 	show question #
+
+// 	user chooses answer
+// 		if right answer
+// 			hide answer choices
+// 			show "correct"
+// 			show definition
+// 			show next button
+
+// 		if wrong answer
+// 			hide answer choices
+// 			show "wrong"
+// 			option to show definition?
+// 			show next button
+
+// 		keep user from submitting with no choices
+
+// 	next question:
+// 		increase question # count
+// 		repeat above
+
+// 	after final question
+// 		show user final score
+// 		"play again" option
+
